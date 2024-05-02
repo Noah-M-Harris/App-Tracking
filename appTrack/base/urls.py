1593d5ead2e68435e4097652aa5287e7665e82bf
Noah Harris <106618581+Noah-M-Harris@users.noreply.github.com>
2024-05-02 12:01:37 -0500

    jQuery sends Applications Information to frontend

--- appTrack/base/urls.py
+++ appTrack/base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('message', views.apps_view, name="message"),
+    path('apps', views.apps_view, name="message"),
 ]
--- appTrack/base/views.py
+++ appTrack/base/views.py
@@ -1,9 +1,14 @@
+from re import S
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core import serializers
+
+from .models import Application
 
 def home(request):
     return render(request, 'home.html')
 
 def apps_view(request):
-    print("\n\nREACHED\n\n")
-    return JsonResponse({'message': 'hello world'})
+    # retrieve user information
+    submitted_applications = serializers.serialize("json", Application.objects.all())
+    return JsonResponse({'applications': submitted_applications, 'message': 'hello world'})
